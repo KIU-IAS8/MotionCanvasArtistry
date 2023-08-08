478a4d524fc329250fdd22fbd7e6de63c699c8ac
samKHa92 <samkha@gfmstudio.com>
2023-08-08 13:12:52 +0200

    Implemented main.py for testing the results

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from tools.display_functions import LiveCapture
+import cv2
 
+from maths.functions.optical_flow import optical_flow
+from tools.display_functions import LiveCapture
+from tools.image_filters import sobel_filter_gradient_magnitude
 
 lc = LiveCapture()
 
-# lc.display_original()
-# lc.display_sobel_filter_x()
-# lc.display_sobel_filter_y()
-
 lc.display_sobel_filter_gradient_magnitude()
 
-# lc.display_morphological_filter_erosion()
-# lc.display_morphological_filter_dilation()
 
-# lc.display_morphological_filter_dilation_sobel_gradient_magnitude()
-# lc.display_morphological_filter_erosion_sobel_gradient_magnitude()
+while True:
+    frame1 = sobel_filter_gradient_magnitude(lc.capture_frames())
+    frame2 = sobel_filter_gradient_magnitude(lc.capture_frames())
+    cv2.imshow(
+        "Live Camera Sobel Gradient Magnitude",
+        cv2.resize(frame1, (1280, 720))
+    )
+    cv2.imshow(
+        "Live Camera Sobel Gradient Magnitude",
+        cv2.resize(frame2, (1280, 720))
+    )
+
+    optical_flow(frame1, frame2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        raise SystemExit
