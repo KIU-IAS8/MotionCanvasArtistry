42f64f715e96ced09963d12dad61de7e0b0bd581
samKHa92 <samkha@gfmstudio.com>
2023-08-30 14:42:22 +0200

    Fixed all the bugs regarding reindexing

--- tools/camera_integration.py
+++ tools/camera_integration.py
@@ -2,25 +2,20 @@ import cv2
 
 
 class VideoCaptureDevice:
-    def __init__(self, device_id=0, width=800, height=800):
+    def __init__(self, device_id=0, width=360, height=360):
         self.video_capture = cv2.VideoCapture(device_id)
-        self.__width = width,
+        self.__width = width
         self.__height = height
 
     def capture_frame(self):
         if self.video_capture.isOpened():
             check, data = self.video_capture.read()
             if check:
-                return self.resize(data, 2.23)
+                return cv2.flip(self.crop(data), 1)
             else:
                 raise Exception("Unable to extract frames")
         else:
             raise Exception("Video capture device is unavailable")
 
-    def resize(self, data, scale):
-        width = int(data.shape[1] * scale)
-        height = int(data.shape[0] * scale)
-        dim = (width, height)
-
-        return cv2.resize(data, dim, interpolation=cv2.INTER_AREA)
-
+    def crop(self, data):
+        return data[:, (data.shape[1] - self.__width) // 2:(data.shape[1] - self.__width) // 2 + self.__width, :]
